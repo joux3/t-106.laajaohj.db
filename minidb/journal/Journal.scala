ab0ad82b68850571aa3530e1ab08e069cec29939
Pekka Poyry <pekka.poyry@aalto.fi>
2011-04-15 15:55:52 +0300

    Added support for nested transactions.

--- minidb/journal/Journal.scala
+++ minidb/journal/Journal.scala
@@ -6,58 +6,119 @@ import minidb.sqlparse._
 import minidb.queryproc._
 import java.io._
 
-// Handles transactions, doesn't support nested/named transactions
-// Journal file contains sql instruction to get the databese from 
-// the begin of this transaction to the current state.
+// Handles transactions, doesn't support named transactions. Journal file structure:
+// First line contains line-numbers for beginnings for all active transactions.
+// Linenumbers start from 1. After that there is sql instruction to get the database
+// to the current state.
 object Journal {
-  // is transaction active
-  var transactionActive = false
+  // how many transactions are active
+  var transactionsActive = 0
+  def transactionActive = transactionsActive > 0
 
   var writer: DataOutputStream = null 
   var journalFileName = "db.journal"
+  //We keep a copy of journal-file in memory
+  var header: Seq[Int] = Seq()
+  var data: Seq[String] = Seq()
 
   def clearJournalFile() {
-      writer.flush
-      writer.close
-      writer = new DataOutputStream(new FileOutputStream(journalFileName))
+    writer.flush
+    writer.close
+    writer = new DataOutputStream(new FileOutputStream(journalFileName))
+  }
+  def updateJournalFile() {
+    clearJournalFile
+    writer writeBytes(header.mkString(" ") + "\n")
+    writer writeBytes(data.mkString("\n"))
+    writer.flush
   }
   def beginTransaction() {
-    if(transactionActive) println("Nested transactions not supported.")
-    else{
-      transactionActive = true;
+    if(transactionsActive == 0)
+    {
       Table.saveToFile
-      clearJournalFile
+      //Line-numbers start from 1 and first line is header
+      header = Seq(2)
     }
+    else
+    {
+      //Mark linenumber of beginning of this transaction
+      //Line-numbers start from 1 and first line is header
+      header = header :+ data.length+2
+    }
+    transactionsActive += 1
+    updateJournalFile
   }
   def commitTransaction() {
-    if(!transactionActive) println("No active transactions.")
-    else{
-      //Save tables to file and clears journal-file
-      Table.saveToFile
-      clearJournalFile
-      transactionActive = false;
+    if(transactionsActive > 0)
+    {
+      if(transactionsActive > 1)
+      {
+        //There are many transactions active, combine last two together
+        header = header.dropRight(1)
+        updateJournalFile
+      }
+      else
+      {
+        //Save tables to file and clears journal-file
+        Table.saveToFile
+        header = Seq()
+        data = Seq()
+        clearJournalFile
+      }
+      transactionsActive -= 1
     }
+    else println("No active transactions.")
   }
   def rollbackTransaction() {
-    Table.loadFromFile
-    clearJournalFile
-    transactionActive = false;
+    if(transactionsActive > 0)
+    {
+      if(transactionsActive > 1)
+      {
+        //There are many transactions active: reload table, filter journal and apply new journal
+        Table.loadFromFile
+        data = data.take(header.last)
+        header = header.dropRight(1)
+        updateJournalFile
+        
+        //This function will update transactionsActive
+        loadTransactionFromFile
+      }
+      else
+      {
+        //Only transaction, remove all data
+        Table.loadFromFile
+        header = Seq()
+        data = Seq()
+        clearJournalFile
+        transactionsActive -= 1
+      }
+    }
+    else println("No active transactions.")
  }
   def loadTransactionFromFile(){
+    var continueJournal = true
     try
     {
-      val lines = scala.io.Source.fromFile(journalFileName).getLines
-      for (line <- lines) {
+      val lines = scala.io.Source.fromFile(journalFileName).getLines.toSeq
+      header = if(lines.length > 0)lines(0).split(' ').filter{line => line.length() > 0}.map(line => line.toInt) else Seq()
+      data = lines.drop(1)
+      for (line <- data) {
         val expr = Parser.parse(line)
         //If file is OK, this won't throw
         QueryProc.processQuery(expr)
       }
-      transactionActive = lines.length > 0
-    }catch{case e: IOException => {}}
-    writer = new DataOutputStream(new FileOutputStream(journalFileName,true))
+      transactionsActive = header.length
+    }catch{
+      case e: IOException => //File not found
+      case _ => {
+        println("Journal file is broken, discarded (data: "+scala.io.Source.fromFile(journalFileName).getLines.mkString("\n")+")")
+        continueJournal = false
+      }
+    }
+    writer = new DataOutputStream(new FileOutputStream(journalFileName,continueJournal))
   }
   def reportCommand(cmd:String, sql: SQLExpr){
-    if(transactionActive){
+    if(transactionsActive > 0){
       sql match {
         //Don't write useless commands
         case BeginTransaction =>
@@ -65,7 +126,10 @@ object Journal {
         case RollbackTransaction =>
         case SimpleSelect(_,_) =>
         case _ => {
-          writer writeBytes(cmd + ";\n")
+          val line = cmd.replace('\n', ' ')
+          writer writeBytes(line + "\n")
+          writer.flush
+          data = data :+ line
         }
       }
     }
--- minidb/tests/TestJournal.scala
+++ minidb/tests/TestJournal.scala
@@ -7,35 +7,48 @@ import java.io._
 
 object TestJournal extends RunnableTest {
   def testTransactions() {
-    //ToDo: this test will destroy current journal file
+    //This test uses different journal file
+    Journal.journalFileName = "test.journal"
     
     //Stupid test, doesn't test just journal
-    Test.startTestSet("Testing journal writing")
+    Test.startTestSet("Testing journal IO")
     //Clear journal file
     val a = new DataOutputStream(new FileOutputStream(Journal.journalFileName))
     
     Journal.loadTransactionFromFile()
     
     def lines = scala.io.Source.fromFile(Journal.journalFileName).getLines.toArray
+    val dymmyLine = "begin tran"
+    val dymmyCommand = dymmyLine+"\n"
 
     Journal.beginTransaction
     Journal.reportCommand("empty",BeginTransaction)
     Journal.reportCommand("empty",CommitTransaction)
     Journal.reportCommand("empty",RollbackTransaction)
     Journal.reportCommand("empty",SimpleSelect(Seq("foo"),CTrue))
-    //var lines = scala.io.Source.fromFile(Journal.journalFileName).getLines
-    Test.assertTrue("Command filttering", lines.length == 0)
+    //Journal contains only header
+    Test.assertTrue("Command filttering", lines.length == 1 && lines(0).equals("2"))
 
-    Journal.reportCommand("foo",SimpleDelete("bar",CTrue))
-    Test.assertTrue("Command writing "+ lines, lines.length == 1 && lines(0).equals("foo;"))
+    Journal.reportCommand(dymmyCommand,SimpleDelete("bar",CTrue))
+    Test.assertTrue("Command writing", lines.length == 2 && lines(1).equals(dymmyLine+" "))
     Journal.rollbackTransaction
-    Test.assertTrue("Rollback clears journal", lines.length == 0)
+    Test.assertTrue("Rollback clears journal", lines.length == 0 && Journal.transactionActive == false)
     
     Journal.beginTransaction
-    Journal.reportCommand("foo",SimpleDelete("bar",CTrue))
+    Journal.reportCommand(dymmyCommand,SimpleDelete("bar",CTrue))
     Journal.commitTransaction
-    Test.assertTrue("Commit clears journal", lines.length == 0)
- 
+    Test.assertTrue("Commit clears journal", lines.length == 0 && Journal.transactionActive == false)
+
+    Journal.beginTransaction
+    Journal.beginTransaction
+    Journal.reportCommand(dymmyCommand,SimpleDelete("bar",CTrue))
+    Test.assertTrue("Nested transactions work", lines.mkString("\n").equals("2 2\n"+dymmyLine+" "))
+    Journal.rollbackTransaction
+    Journal.rollbackTransaction
+    Test.finishTestSet()
+    
+    Test.startTestSet("Testing journal <-> table interaction")
+
     //This will test way more than needed
     def start() = {
       Journal.beginTransaction
@@ -57,7 +70,10 @@ object TestJournal extends RunnableTest {
     try{
       val res =  QueryProc.processQuery(SimpleSelect(Seq("foo"),CTrue))
     }catch{case e: QueryProcException => {ok = false}}
-    Test.assertTrue("Commit keeps database valid",ok)
+    Test.assertTrue("Commit keeps database intact",ok)
+    
+    //Remove journal file
+    new File(Journal.journalFileName) delete
   
     Test.finishTestSet()
   }
